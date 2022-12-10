5be8a8ac9bea57592c6cc06338b5672e318c3d89
Drayzd <g31plus@gmail.com>
2022-12-10 19:02:13 +0100

    création interfaces

---
+++ src/fc/interfaces/FCInterface.java
@@ -0,0 +1,66 @@
+package fc.interfaces;
+import IHM.App;
+import fc.*;
+
+import java.util.ArrayList;
+
+public class FCInterface {
+    App a;
+    CyberVideoInterface cvi;
+    DBInterface dbi;
+
+    public FCInterface(App a, CyberVideoInterface cvi, DBInterface dbi){
+        this.a = a;
+        this.cvi = cvi;
+        this.dbi = dbi;
+    }
+
+
+    //TODO: implement methods
+    public void getAvailableMovies(){
+
+    }
+    public ArrayList<String> getMoviesAvailableAsBluRay() {
+        return new ArrayList<String>();
+    }
+    public void getMoviesAvailableAsQRCode(){
+
+    }
+    public void  searchUnavailableMovie(String s){
+
+    }
+    public void  getWeeklyTopRentals(Integer i){
+
+    }
+    public void  getMonthlyTopRentals(Integer i){
+
+    }
+    public void  rentAsBluRay(Movie m){
+
+    }
+    public void  rentAsQRCode(Movie m){
+
+    }
+    public void  requestMovieAsBluRay(Movie m){
+
+    }
+    public void  topUpCard(){
+
+    }
+    public void  getHistory(){
+
+    }
+    public void  getBalance() {
+
+    }
+    public void  login(){
+
+    }
+    public void  logout(){
+
+    }
+    public void  createAccount(){
+
+    }
+
+}
