2121d13aca2604487266bacd695638d4f3c5ca11
delphineGUTFREUND <delphine.gutfreund@free.fr>
2022-12-10 18:11:35 +0100

    getter/setter ( CategorieRestrictions et Categories )

--- src/fc/Categories.java
+++ src/fc/Categories.java
@@ -1,4 +1,15 @@
 package fc;
 
 public abstract class Categories {
+
+    private String category;
+
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
