e166b3ebfad4066b2a4b687f867dd24e39ec6b16
Drayzd <g31plus@gmail.com>
2022-12-11 21:25:22 +0100

    ajout méthodes au bon endroit

--- src/dao/DAOFacadeMovie.java
+++ src/dao/DAOFacadeMovie.java
@@ -168,4 +168,78 @@ public class DAOFacadeMovie {
         return false;
     }
 
+    public ArrayList<Movie> getMovieList(){ // All available movies
+
+        ArrayList<Movie> movies = new ArrayList<>();
+
+        try {
+            PreparedStatement MovieStatement = conn.prepareStatement("SELECT * FROM Movies_base");
+            ResultSet MovieResult = MovieStatement.executeQuery();
+
+            while(MovieResult.next()) {
+                Movie movie = new Movie();
+
+                movie.setId(MovieResult.getInt(1));
+                movie.setTitle(MovieResult.getString(2));
+                movie.setDirectorLastname(MovieResult.getString(3));
+                movie.setDirectorFirstname(MovieResult.getString(4));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return movies;
+    }
+
+    public ArrayList<Movie> getMovieListQR(){ // All movies available on QR Code
+
+        ArrayList<Movie> movies = new ArrayList<>();
+
+        try {
+            PreparedStatement MovieStatement = conn.prepareStatement("SELECT * FROM Movies_base WHERE movieID IN " +
+                    "(SELECT * FROM SUPPORTS WHERE supportType = 'QRCode')");
+            ResultSet MovieResult = MovieStatement.executeQuery();
+
+            while(MovieResult.next()) {
+                Movie movie = new Movie();
+
+                movie.setId(MovieResult.getInt(1));
+                movie.setTitle(MovieResult.getString(2));
+                movie.setDirectorLastname(MovieResult.getString(3));
+                movie.setDirectorFirstname(MovieResult.getString(4));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return movies;
+    }
+
+    public ArrayList<Movie> getMovieListBR(){ // All movies available on BluRay
+
+        ArrayList<Movie> movies = new ArrayList<>();
+
+        try {
+            PreparedStatement MovieStatement = conn.prepareStatement("SELECT * FROM Movies_base WHERE movieID IN " +
+                    "(SELECT * FROM SUPPORTS WHERE supportType = 'BluRay' and available = 1)");
+            ResultSet MovieResult = MovieStatement.executeQuery();
+
+            while(MovieResult.next()) {
+                Movie movie = new Movie();
+
+                movie.setId(MovieResult.getInt(1));
+                movie.setTitle(MovieResult.getString(2));
+                movie.setDirectorLastname(MovieResult.getString(3));
+                movie.setDirectorFirstname(MovieResult.getString(4));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return movies;
+    }
+
 }
--- src/fc/interfaces/DBInterface.java
+++ src/fc/interfaces/DBInterface.java
@@ -29,82 +29,20 @@ public class DBInterface {
             facadeUser = new DAOFacadeUser(conn);
         }
 
-
-
-
-    public ArrayList<Movie> getMovieList(){ // All available movies
-        // A METTRE DANS DAOFacadeMovie
-        ArrayList<Movie> movies = new ArrayList<>();
-
-        try {
-            PreparedStatement MovieStatement = conn.prepareStatement("SELECT * FROM Movies_base");
-            ResultSet MovieResult = MovieStatement.executeQuery();
-
-            while(MovieResult.next()) {
-                Movie movie = new Movie();
-
-                movie.setId(MovieResult.getInt(1));
-                movie.setTitle(MovieResult.getString(2));
-                movie.setDirectorLastname(MovieResult.getString(3));
-                movie.setDirectorFirstname(MovieResult.getString(4));
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+        public ArrayList<Movie> getMovieList(){
+            return facadeMovie.getMovieList();
         }
 
-        return movies;
-    }
-
-    public ArrayList<Movie> getMovieListQR(){ // All movies available on QR Code
-        // A METTRE DANS DAOFacadeMovie
-        ArrayList<Movie> movies = new ArrayList<>();
-
-        try {
-            PreparedStatement MovieStatement = conn.prepareStatement("SELECT * FROM Movies_base WHERE movieID IN " +
-                    "(SELECT * FROM SUPPORTS WHERE supportType = 'QRCode')");
-            ResultSet MovieResult = MovieStatement.executeQuery();
-
-            while(MovieResult.next()) {
-                Movie movie = new Movie();
-
-                movie.setId(MovieResult.getInt(1));
-                movie.setTitle(MovieResult.getString(2));
-                movie.setDirectorLastname(MovieResult.getString(3));
-                movie.setDirectorFirstname(MovieResult.getString(4));
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+        public ArrayList<Movie> getMovieListQR(){
+            return facadeMovie.getMovieListQR();
         }
 
-        return movies;
-    }
-
-    public ArrayList<Movie> getMovieListBR(){ // All movies available on BluRay
-        // A METTRE DANS DAOFacadeMovie
-        ArrayList<Movie> movies = new ArrayList<>();
-
-        try {
-            PreparedStatement MovieStatement = conn.prepareStatement("SELECT * FROM Movies_base WHERE movieID IN " +
-                    "(SELECT * FROM SUPPORTS WHERE supportType = 'BluRay' and available = 1)");
-            ResultSet MovieResult = MovieStatement.executeQuery();
-
-            while(MovieResult.next()) {
-                Movie movie = new Movie();
-
-                movie.setId(MovieResult.getInt(1));
-                movie.setTitle(MovieResult.getString(2));
-                movie.setDirectorLastname(MovieResult.getString(3));
-                movie.setDirectorFirstname(MovieResult.getString(4));
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+        public ArrayList<Movie> getMovieListBR(){
+            return facadeMovie.getMovieListBR();
         }
 
-        return movies;
-    }
+
+
 
 
 
