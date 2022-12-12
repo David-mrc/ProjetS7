aaf4dd72a43d0ccb3f86a9efdbf02518c91d72ac
dcs-b <81160533+dcs-b@users.noreply.github.com>
2022-12-12 21:48:14 +0100

    Changement AccountScreen de ScrollScreen à Screen

--- src/ui/AccountScreen.java
+++ src/ui/AccountScreen.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
-public class AccountScreen extends ScrollScreen {
+public class AccountScreen extends Screen {
     FCInterface fc;
 
 
@@ -17,7 +17,7 @@ public class AccountScreen extends ScrollScreen {
         this.fc = fc;
 
         JLabel userInfo = createTitle("User Information - ");
-        pane.add(userInfo, BorderLayout.NORTH);
+        add(userInfo, BorderLayout.NORTH);
 
         User currentUser =  fc.getUser();
         JLabel name = createText("Name  -  " + currentUser.getFirstname() + " " + currentUser.getLastName());
@@ -28,7 +28,7 @@ public class AccountScreen extends ScrollScreen {
         } else {
             address = createText("Address  -  " + " " + "Not Given");
         }
-        pane.add(address, BorderLayout.CENTER);
+        add(address, BorderLayout.CENTER);
 
         JLabel dob;
         if(currentUser.getBirthday() != null){
@@ -36,15 +36,15 @@ public class AccountScreen extends ScrollScreen {
         } else {
             dob = createText("Date of Birth  -  " + " " + "Not Given");
         }
-        pane.add(dob, BorderLayout.CENTER);
+        add(dob, BorderLayout.CENTER);
 
         if(currentUser.isSubscriber()) {
             JLabel sub = createText("Proud Subscriber ! ");
-            pane.add(sub, BorderLayout.CENTER);
+            add(sub, BorderLayout.CENTER);
         }
 
         JLabel history = createTitle("Past Rentals - ");
-        pane.add(history, BorderLayout.CENTER);
+        add(history, BorderLayout.CENTER);
 
         ArrayList<Movie> historyArray = fc.getHistory();
 
@@ -54,16 +54,9 @@ public class AccountScreen extends ScrollScreen {
             movieButton.addActionListener(e -> window.openMovieScreen(m));
             historyPane.add(movieButton);
         }
-        pane.add(historyPane, BorderLayout.CENTER);
+        add(historyPane, BorderLayout.CENTER);
 
         //TODO: add card status.
-    }
-
-    private JLabel createTitle(String title) {
-        JLabel label = new JLabel(title);
-        label.setForeground(Color.ORANGE);
-        label.setFont(new Font("Arial", Font.PLAIN, 30));
-        return label;
     }
 
     private JLabel createText(String text) {
