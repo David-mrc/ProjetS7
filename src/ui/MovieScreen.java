6694a8365c936c955fe2dfcc335cc16766467349
dcs-b <81160533+dcs-b@users.noreply.github.com>
2022-12-12 20:49:41 +0100

    Ajout MovieScreen

---
+++ src/ui/MovieScreen.java
@@ -0,0 +1,15 @@
+package ui;
+
+import fc.Movie;
+import fc.interfaces.FCInterface;
+
+import java.awt.*;
+
+public class MovieScreen extends Screen {
+
+    MovieScreen(Window window, FCInterface fc, Movie movie) {
+        super(new GridBagLayout());
+
+
+    }
+}
