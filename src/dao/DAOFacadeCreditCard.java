053713a1c24f53b81826021f8000220a6108cf91
Drayzd <g31plus@gmail.com>
2022-12-12 17:19:00 +0100

    implem rentmovie bluray/QRCode, subscriber/ non-subscriber avec Credit Card/SubscriberCard

--- src/dao/DAOFacadeCreditCard.java
+++ src/dao/DAOFacadeCreditCard.java
@@ -1,6 +1,11 @@
 package dao;
 
+import fc.CreditCard;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DAOFacadeCreditCard {
     protected Connection conn;
@@ -13,6 +18,31 @@ public class DAOFacadeCreditCard {
         System.out.println("...");
         System.out.println("Authorized payment !");
         return true;
+    }
+
+    public CreditCard getCreditCard(int userID){
+        CreditCard cc = new CreditCard();
+        try {
+
+            PreparedStatement CCStatement = conn.prepareStatement("SELECT * FROM CREDITCARDS WHERE USERID = ?");
+            CCStatement.setInt(1, userID);
+            ResultSet resultSet = CCStatement.executeQuery();
+
+
+            if (resultSet.next()) {
+
+                    cc.setCardNumber(resultSet.getInt(1));
+                    cc.setHolder(resultSet.getString(2));
+                    cc.setCcv(resultSet.getInt(3));
+                    cc.setExpiryDate(resultSet.getDate(4));
+                    cc.setUserID(resultSet.getInt(5));
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return cc;
     }
 }
 
